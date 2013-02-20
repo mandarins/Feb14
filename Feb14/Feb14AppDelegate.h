81a5f984f47a20689c1acfda3a9e2d889c20e33a
Salvatore Lentini <mandarins@gmail.com>
2013-02-19 21:12:06 -0500

    Initial Commit

---
+++ Feb14/Feb14AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Feb14AppDelegate.h
+//  Feb14
+//
+//  Created by Salvatore Lentini on 2/19/13.
+//  Copyright (c) 2013 Salvatore Lentini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Feb14AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Feb14/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Feb14
+//
+//  Created by Salvatore Lentini on 2/19/13.
+//  Copyright (c) 2013 Salvatore Lentini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Feb14AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Feb14AppDelegate class]));
+    }
+}
